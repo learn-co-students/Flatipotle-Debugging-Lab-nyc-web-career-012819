f9ce667687c8d14db2098d79f0f254681f7bbe9f
github username <github email address>
2019-04-05 14:43:52 -0400

    completed lab

--- src/components/Side.js
+++ src/components/Side.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Side = (props) => {
-  return (
-    <div>
-      { props.sides.join(", ") }
-    </div>
-  )
+class Side extends React.Component {
+  render() {
+    console.log('in side');
+    return (
+      <div>
+        { this.props.sides.join(", ") }
+      </div>
+    )
+  }
 }
 
 export default Side
